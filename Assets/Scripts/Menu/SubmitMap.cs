1dbb00621e4ed5eb5f19830b021038de23be4f60
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2026-04-03 19:03:33 +0100

    Map can be resubmitted without breaking files structure
    If you resubmitting the map replaces the leaderboardID and does not add extra blank lines

--- Assets/Scripts/Menu/SubmitMap.cs
+++ Assets/Scripts/Menu/SubmitMap.cs
@@ -2,7 +2,9 @@ using SFB;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class SubmitMap : MonoBehaviour
@@ -30,11 +32,32 @@ public class SubmitMap : MonoBehaviour
         int secondLastPos = loader.url.LastIndexOf('/', lastPos - 1);
 
         txt = File.ReadAllText(Application.persistentDataPath + "/Music/" + loader.url.Substring(secondLastPos + 1, loader.url.Substring(secondLastPos + 1).Length));
+        string[] lines = txt.Split("\n");
+        if (lines[lines.Length-1].Contains("LeaderboardID:"))
+        {
+            StreamWriter sw = new StreamWriter(url, false);
+            lines[lines.Length-1] = "LeaderboardID:" + System.DateTime.UtcNow.Ticks;
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                sw.Write(lines[i]);
+                sw.Write("\n");
+            }
 
-        txt =txt + "\nLeaderboardID:" + System.DateTime.UtcNow.Ticks;
-        Debug.Log(url);
-        StreamWriter sw = new StreamWriter(url, true);
-        sw.Write("\nLeaderboardID:" + System.DateTime.UtcNow.Ticks);
-        sw.Close();
+                sw.Write(lines[lines.Length - 1]);
+            
+
+            sw.Close();
+
+        }
+        else
+        {
+            Debug.Log(lines[lines.Length-1]); 
+            txt = txt + "\nLeaderboardID:" + System.DateTime.UtcNow.Ticks;
+            Debug.Log(url);
+            StreamWriter sw = new StreamWriter(url, true);
+            sw.Write("\nLeaderboardID:" + System.DateTime.UtcNow.Ticks);
+            sw.Close();
+        }
+
     }
 }
